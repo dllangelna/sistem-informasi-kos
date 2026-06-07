f3f3e2fac59d6843e5ca7991a94adf4a97840310
Dilla Angelina <dillaangelina2006@gmail.com>
2026-06-07 11:49:00 +0700

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Informasi Kos</title>
+</head>
+<body>
+    <h1>Selamat Datang di Sistem Informasi Pengelolaan Kos</h1>
+    <nav>
+        <ul>
+            <li><a href="views/kamar_view.php">Kelola Data Kamar</a></li>
+            <li><a href="views/penghuni_view.php">Kelola Data Penghuni</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
